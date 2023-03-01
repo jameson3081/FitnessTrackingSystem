4ed055834da59b6fe9addb10cbf111b458e03846
jameson3081 <jamesonlouis.celzo.cics@ust.edu.ph>
2023-03-01 13:42:53 +0800

    added mongoDB Server connection

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const path = require ('path');
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 //Set EJS as the View engine
 app.set('view engine', 'ejs');
@@ -8,6 +12,28 @@ app.set('view engine', 'ejs');
 //IMPORT ROUTES(/routes/)
 const pagesRoute = require('./routes/pages')
 app.use(express.static(path.join('public'))) //USE files in 'public' folder
+
+
+
+//ESTABLISHING MONGODB NODESERVER CONNECTION(CHECK .env)
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true} )
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('MongoDB Server Database Connection Established...')
+})
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+// END OF ESTABLISHING MONGODB CONNECTION
+
+
+
 
 
 
