8f8af88c83064341902436f8012d85f9f3c16898
Ansly21 <anslybrandon.dy.iics@ust.edu.ph>
2023-03-07 10:14:49 +0800

    added new model

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const UserSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
